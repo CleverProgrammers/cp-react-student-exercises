cb36ce5642bc4ed9e3a67ec079d17d7ac947b592
Rafeh Qazi <qazi@cleverprogrammer.com>
2021-01-19 11:03:26 -0800

    Add basic react routing functionality to switch between exercises.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import useWindowSize from "react-use/lib/useWindowSize";
 import Confetti from "react-confetti";
 import { Box, Button, Grid } from "@material-ui/core";
 import { sum } from "./exercises/1-sum";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -25,34 +26,55 @@ const App = () => {
   };
 
   return (
-    <Grid
-      container
-      spacing={24}
-      direction="row"
-      alignItems="center"
-      justify="center"
-      style={{ minHeight: "100vh", backgroundColor: "#1a1f36" }}
-    >
-      <Button variant="contained" color="primary" onClick={handleClick}>
-        TEST MY CODE
-      </Button>
-      <Box m={1} />
-      <Button
-        variant="contained"
-        color="default"
-        onClick={handleCloseModalClick}
-      >
-        Clear
-      </Button>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/1-exercise">Exercise 1</Link>
+        </li>
+        <li>
+          <Link to="/2-exercise">Exercise 2</Link>
+        </li>
+      </ul>
+      <hr />
 
-      <Toaster />
-      {modalOpen && (
-        <>
-          <TransitionsModal isModalOpen={modalOpen} />
-          <Confetti width={width} height={height} />
-        </>
-      )}
-    </Grid>
+      <Switch>
+        <Route path="/1-exercise">
+          <Grid
+            container
+            spacing={24}
+            direction="row"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: "100vh", backgroundColor: "#1a1f36" }}
+          >
+            <Button variant="contained" color="primary" onClick={handleClick}>
+              TEST MY CODE
+            </Button>
+            <Box m={1} />
+            <Button
+              variant="contained"
+              color="default"
+              onClick={handleCloseModalClick}
+            >
+              Clear
+            </Button>
+
+            <Toaster />
+            {modalOpen && (
+              <>
+                <TransitionsModal isModalOpen={modalOpen} />
+                <Confetti width={width} height={height} />
+              </>
+            )}
+          </Grid>
+        </Route>
+
+        <Route path="/sum"></Route>
+      </Switch>
+    </Router>
   );
 };
 
